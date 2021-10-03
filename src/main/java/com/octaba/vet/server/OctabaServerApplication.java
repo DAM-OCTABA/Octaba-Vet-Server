285e2d8b9000b81b576d339ffc76fc93d4d9631b
Oscar Rovira <560186+krosk93@users.noreply.github.com>
2021-10-03 20:34:10 +0200

    Initial Commit

---
+++ src/main/java/com/octaba/vet/server/OctabaServerApplication.java
@@ -0,0 +1,13 @@
+package com.octaba.vet.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OctabaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OctabaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/octaba/vet/server/OctabaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.octaba.vet.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OctabaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
